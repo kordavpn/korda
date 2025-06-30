5ce46eb3d0be9c170a16138cf0eee7d0205d28fb
kordavpn <130245138+kordavpn@users.noreply.github.com>
2025-06-30 23:38:28 +0330

    Update korda.sh

--- korda.sh
+++ korda.sh
@@ -89,7 +89,7 @@ config_after_install() {
 
     if [[ ${#existing_webBasePath} -lt 4 ]]; then
         if [[ "$existing_hasDefaultCredential" == "true" ]]; then
-            local config_port="1400:
+            local config_port="1400"
             local config_username="k"
             local config_password="k"
 
